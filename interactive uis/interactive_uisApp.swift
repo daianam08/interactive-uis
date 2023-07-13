de9a75efb18cbb80a17a9e674be3889c76be321f
DaianaM <daiana.m.0708@gmail.com>
2023-07-13 11:12:09 -0700

    Initial Commit

---
+++ interactive uis/interactive_uisApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_uisApp.swift
+//  interactive uis
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct interactive_uisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

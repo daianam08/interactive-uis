2e5e8793e8095bb5c6e3b0eba95c932e97de6d62
DaianaM <daiana.m.0708@gmail.com>
2023-07-13 13:43:09 -0700

    done

--- interactive uis/ContentView.swift
+++ interactive uis/ContentView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        VStack{
+            Text(textTitle)
+                .font(.title)
+            
+            TextField("Type name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title2)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit"){
+                textTitle = "Welcome, \(name)!"
+                
+            }
+        //button
+            .font(.title3)
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+        
+        }//vstack
         .padding()
     }
 }
